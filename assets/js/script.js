3b49c212a0fdafd1b367d643388b9dab8f5e140a
AnjinIsmail <anjin.ismail@gmail.com>
2020-10-07 21:17:36 -0700

    form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-buttonEl.addEventListener("click" , createTaskHandler); {
+formEl.addEventListener("submit" , createTaskHandler); {
     var listItemEl = document.createElement ("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task." ;
@@ -9,9 +9,11 @@ buttonEl.addEventListener("click" , createTaskHandler); {
 };
 
 // basically created a new task item, styled the new task item , added txt and appended this element to the task list
-var createTaskHandler= function () {
+var createTaskHandler= function (event) {
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className="task-item";
     listItemEl.textContent =" This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-}
+};

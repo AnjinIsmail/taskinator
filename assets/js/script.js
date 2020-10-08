b08d0daab09d21337466beb4b8e496c771f3db65
AnjinIsmail <anjin.ismail@gmail.com>
2020-10-08 12:35:35 -0700

    form-submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,22 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-formEl.addEventListener("submit" , createTaskHandler); {
-    var listItemEl = document.createElement ("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task." ;
-    tasksToDoEl.appendChild(listItemEl);
-};
-
 // basically created a new task item, styled the new task item , added txt and appended this element to the task list
-var createTaskHandler= function (event) {
-    event.preventDefault();
-
-    var listItemEl = document.createElement("li");
-    listItemEl.className="task-item";
-    listItemEl.textContent =" This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
+var createTaskHandler = function (event) {
+  event.preventDefault();
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task";
+  tasksToDoEl.appendChild(listItemEl);
 };
+
+var taskNameInput = document.querySelector("input[name='task-name']");
+console.log(taskNameInput);
+
+formEl.addEventListener("submit", createTaskHandler);
+{
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+}
